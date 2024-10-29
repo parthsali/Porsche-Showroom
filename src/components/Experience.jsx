838d5c5a1fcc31ebc66274aab16785822493e3df
ParthSali <parthsali04@gmail.com>
2024-10-29 22:31:26 +0530

    Final changes

---
+++ src/components/Experience.jsx
@@ -0,0 +1,173 @@
+import {
+  CameraControls,
+  Dodecahedron,
+  Environment,
+  Grid,
+  MeshDistortMaterial,
+  RenderTexture,
+} from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useAtom } from "jotai";
+import { useControls } from "leva";
+import { useEffect, useRef } from "react";
+import { slideAtom } from "./Overlay";
+import { Scene } from "./Scene";
+
+export const scenes = [
+  {
+    path: "models/carrera_scene.glb",
+    mainColor: "#c0ffe1",
+    name: "Porsche 911 Carrera S",
+    description:
+      "Classic Porsche rear-engine design for sportiness and daily use.",
+    price: 15000000, // Approx. ₹1.5 crore
+    acceleration: "3.5", // 0-100 km/h in 3.5 seconds
+  },
+  {
+    path: "models/clubsport_scene.glb",
+    mainColor: "#ffdec0",
+    name: "GT4 RS Clubsport",
+    description:
+      "Track-ready with a 4.0L flat-six engine, tailored for precision handling.",
+    price: 16000000, // Estimated ₹1.6 crore
+    acceleration: "3.4", // 0-100 km/h in 3.4 seconds
+  },
+  {
+    path: "models/turbo.glb",
+    mainColor: "#f9c0ff",
+    name: "2016 Porsche 911 Turbo S",
+    description:
+      "Luxury meets track performance with a 3.7L twin-turbo flat-six engine.",
+    price: 25000000, // Approx. ₹2.5 crore
+    acceleration: "2.7", // 0-100 km/h in 2.7 seconds
+  },
+];
+
+const CameraHandler = ({ slideDistance }) => {
+  const viewport = useThree((state) => state.viewport);
+  const cameraControls = useRef();
+  const [slide] = useAtom(slideAtom);
+  const lastSlide = useRef(0);
+
+  const { dollyDistance } = useControls({
+    dollyDistance: {
+      value: 10,
+      min: 0,
+      max: 50,
+    },
+  });
+
+  const moveToSlide = async () => {
+    await cameraControls.current.setLookAt(
+      lastSlide.current * (viewport.width + slideDistance),
+      3,
+      dollyDistance,
+      lastSlide.current * (viewport.width + slideDistance),
+      0,
+      0,
+      true
+    );
+    await cameraControls.current.setLookAt(
+      (slide + 1) * (viewport.width + slideDistance),
+      1,
+      dollyDistance,
+      slide * (viewport.width + slideDistance),
+      0,
+      0,
+      true
+    );
+
+    await cameraControls.current.setLookAt(
+      slide * (viewport.width + slideDistance),
+      0,
+      5,
+      slide * (viewport.width + slideDistance),
+      0,
+      0,
+      true
+    );
+  };
+
+  useEffect(() => {
+    // Used to reset the camera position when the viewport changes
+    const resetTimeout = setTimeout(() => {
+      cameraControls.current.setLookAt(
+        slide * (viewport.width + slideDistance),
+        0,
+        5,
+        slide * (viewport.width + slideDistance),
+        0,
+        0
+      );
+    }, 200);
+    return () => clearTimeout(resetTimeout);
+  }, [viewport]);
+
+  useEffect(() => {
+    if (lastSlide.current === slide) {
+      return;
+    }
+    moveToSlide();
+    lastSlide.current = slide;
+  }, [slide]);
+  return (
+    <CameraControls
+      ref={cameraControls}
+      touches={{
+        one: 0,
+        two: 0,
+        three: 0,
+      }}
+      mouseButtons={{
+        left: 0,
+        middle: 0,
+        right: 0,
+      }}
+    />
+  );
+};
+
+export const Experience = () => {
+  const viewport = useThree((state) => state.viewport);
+  const { slideDistance } = useControls({
+    slideDistance: {
+      value: 1,
+      min: 0,
+      max: 10,
+    },
+  });
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <Environment preset={"city"} />
+      <CameraHandler slideDistance={slideDistance} />
+      {/* MAIN WORLD */}
+
+      <Grid
+        position-y={-viewport.height / 2}
+        sectionSize={1}
+        sectionColor={"purple"}
+        sectionThickness={1}
+        cellSize={0.5}
+        cellColor={"#6f6f6f"}
+        cellThickness={0.6}
+        infiniteGrid
+        fadeDistance={50}
+        fadeStrength={5}
+      />
+      {scenes.map((scene, index) => (
+        <mesh
+          key={index}
+          position={[index * (viewport.width + slideDistance), 0, 0]}
+        >
+          <planeGeometry args={[viewport.width, viewport.height]} />
+          <meshBasicMaterial toneMapped={false}>
+            <RenderTexture attach="map">
+              <Scene {...scene} />
+            </RenderTexture>
+          </meshBasicMaterial>
+        </mesh>
+      ))}
+    </>
+  );
+};
